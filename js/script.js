6b0ef47ce9fcbb63c84ba3768b9390b83f92728c
LuanB-compt <luan-bruno-domingues@hotmail.com>
2022-08-07 17:48:14 -0300

    Fixed some bugs to search the pokemons in script.js

--- js/script.js
+++ js/script.js
@@ -8,23 +8,29 @@ const input = document.querySelector('.input__search');
 
 const fetchPokemon = async (pokemon) => {
   const APIResnponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-  const data = await APIResnponse.json();
-  return data;
+  if (APIResnponse.status == 200) {
+    const data = await APIResnponse.json();
+    return data;
+  }
 }
 
 const renderPokemon = async (pokemon) => {
+  pokemon_name.innerHTML = 'Loading ...';
+  pokemon_number.innerHTML = '';
   const data = await fetchPokemon(pokemon);
-
-  pokemon_name.innerHTML = data.name;
-  pokemon_number.innerHTML = data.id;
-  pokemon_img.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-
-  input.value = '';
+  if (data) {
+    pokemon_name.innerHTML = data.name;
+    pokemon_number.innerHTML = data.id;
+    pokemon_img.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    input.value = '';
+  } else {
+    pokemon_name.innerHTML = 'Not Found bro :c';
+    pokemon_number.innerHTML = '';
+  }
 }
 
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-
   renderPokemon(input.value.toLowerCase());
 });
